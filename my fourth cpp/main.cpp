9a3b17b2ec35a03852697405dd96ab92df38dda6
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2021-07-23 05:52:38 +0700

    Initial Commit

---
+++ my fourth cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  my fourth cpp
+//
+//  Created by Rafi Izzatul Rizqu Faris on 23/07/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
