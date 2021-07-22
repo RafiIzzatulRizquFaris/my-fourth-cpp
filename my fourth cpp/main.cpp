6518d4a7972727f86d97e9a2000e4b516c4abb11
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2021-07-23 06:03:19 +0700

    add task 1

--- my fourth cpp/main.cpp
+++ my fourth cpp/main.cpp
@@ -8,7 +8,31 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    std::string nama, kampus;
+    int mahasiswaNow, mahasiswaFuture, birthYear;
+    
+    std::cout << "Nama dosen : ";
+    getline(std::cin, nama);
+    std::cout << std::endl;
+    
+    std::cout << "Mengajar di kampus : ";
+    getline(std::cin, kampus);
+    std::cout << std::endl;
+    
+    std::cout << "Jumlah mahasiswa tahun ini : ";
+    std::cin >> mahasiswaNow;
+    std::cout << std::endl;
+    
+    std::cout << "Rencana jumlah mahasiswa tahun depan : ";
+    std::cin >> mahasiswaFuture;
+    std::cout << std::endl;
+    
+    std::cout << "Tahun lahir : ";
+    std::cin >> birthYear;
+    std::cout << std::endl;
+    
+    std::cout << "Halo, nama saya " << nama << ". Saat ini saya mengajar di kampus " << kampus << ". Saya berusia " << 2021 - birthYear << " dan tahun ini saya mengajar sebanyak " << mahasiswaNow << " mahasiswa. Tahun depan yaitu tahun 2022 saya berusia " << 2022 - birthYear << " dan akan berupaya memiliki jumlah mahasiswa sebanyak " << mahasiswaFuture << std::endl;
+    
     return 0;
 }
