8c17300af2e0572a26dc033f6caa9b6f5b8f1c5d
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2021-07-23 07:08:50 +0700

    add task 4

--- my fourth cpp/main.cpp
+++ my fourth cpp/main.cpp
@@ -90,55 +90,102 @@ int main(int argc, const char * argv[]) {
 //    std::cout << "Diskon : " << diskon << std::endl;
 //    std::cout << "total Bayar : " << totalHarga << std::endl;
     
-    int jamParkir = 0;
-    int jenisKendaraan;
-    int denda = 0;
-    int totalBiaya = 0;
+//    int jamParkir = 0;
+//    int jenisKendaraan;
+//    int denda = 0;
+//    int totalBiaya = 0;
+//
+//        std::cout << "Jumlah total jam parkir : ";
+//        std::cin >> jamParkir;
+//        std::cout << std::endl;
+//
+//        std::cout << "Jenis Kendaraan : \n1. Mobil\n2. Motor" << std::endl;
+//        std::cin >> jenisKendaraan;
+//        std::cout << std::endl;
+//
+//    if (jenisKendaraan == 1) {
+//        totalBiaya = 5000;
+//        if (jamParkir > 2) {
+//            totalBiaya += (jamParkir - 2) * 3000;
+//        }
+//
+//        if (totalBiaya > 20000) {
+//            totalBiaya = 20000;
+//        }
+//
+//        if (jamParkir > 24) {
+//            denda = (jamParkir - 24) * 3000;
+//            totalBiaya += denda;
+//        }
+//    } else {
+//        totalBiaya = 3000;
+//        if (jamParkir > 2) {
+//            totalBiaya += (jamParkir - 2) * 1000;
+//        }
+//
+//        if (totalBiaya > 10000) {
+//            totalBiaya = 10000;
+//        }
+//
+//        if (jamParkir > 24) {
+//            denda = (jamParkir - 24) * 1000;
+//            totalBiaya += denda;
+//        }
+//    }
+//
+//
+//    std::string kendaraan = (jenisKendaraan == 1) ? "Mobil" : "Motor";
+//    std::cout << "Jenis kendaraan : " << kendaraan << std::endl;
+//    std::cout << "Jumlah jam : " << jamParkir << std::endl;
+//    std::cout << "Denda : " << denda << std::endl;
+//    std::cout << "Total biaya : " << totalBiaya << std::endl;
     
-        std::cout << "Jumlah total jam parkir : ";
-        std::cin >> jamParkir;
-        std::cout << std::endl;
     
-        std::cout << "Jenis Kendaraan : \n1. Mobil\n2. Motor" << std::endl;
-        std::cin >> jenisKendaraan;
-        std::cout << std::endl;
+    int jamKerja = 0;
+    int gajiPokok = 0;
+    int transportasi = 0;
+    int makan = 0;
+    int totalGaji = 0;
     
-    if (jenisKendaraan == 1) {
-        totalBiaya = 5000;
-        if (jamParkir > 2) {
-            totalBiaya += (jamParkir - 2) * 3000;
-        }
-        
-        if (totalBiaya > 20000) {
-            totalBiaya = 20000;
-        }
-        
-        if (jamParkir > 24) {
-            denda = (jamParkir - 24) * 3000;
-            totalBiaya += denda;
-        }
-    } else {
-        totalBiaya = 3000;
-        if (jamParkir > 2) {
-            totalBiaya += (jamParkir - 2) * 1000;
-        }
-        
-        if (totalBiaya > 10000) {
-            totalBiaya = 10000;
-        }
-        
-        if (jamParkir > 24) {
-            denda = (jamParkir - 24) * 1000;
-            totalBiaya += denda;
-        }
-    }
+    std::cout << "Jumlah Jam Kerja dalam Sehari (Senin sd Jumat) : ";
+    std::cin >> jamKerja;
+    std::cout << std::endl;
+
+    std::cout << "Gaji Pokok Per Jam : ";
+    std::cin >> gajiPokok;
+    std::cout << std::endl;
     
+    std::cout << "Tunjangan Transport : ";
+    std::cin >> transportasi;
+    std::cout << std::endl;
     
-    std::string kendaraan = (jenisKendaraan == 1) ? "Mobil" : "Motor";
-    std::cout << "Jenis kendaraan : " << kendaraan << std::endl;
-    std::cout << "Jumlah jam : " << jamParkir << std::endl;
-    std::cout << "Denda : " << denda << std::endl;
-    std::cout << "Total biaya : " << totalBiaya << std::endl;
+    std::cout << "Tunjangan Makan : ";
+    std::cin >> makan;
+    std::cout << std::endl;
+    
+    totalGaji = (jamKerja * 5 * 4 * gajiPokok) + transportasi + makan;
+    int sedekah = totalGaji * 10 / 100;
+    int atk = totalGaji * 3 / 100;
+    int pakaian = totalGaji * 10 / 100;
+    int menabung = totalGaji * 10 / 100;
+    int listrik = 200000;
+    
+    totalGaji -= sedekah;
+    std::cout << "Sisa gaji setelah dipotong sedekah 10% : " << totalGaji << std::endl;
+    
+    totalGaji -= atk;
+    std::cout << "Sisa gaji setelah membeli atk 3% : " << totalGaji << std::endl;
+    
+    totalGaji -= pakaian;
+    std::cout << "Sisa gaji setelah membeli pakaian sebesar 10% : " << totalGaji << std::endl;
+    
+    totalGaji -= listrik;
+    std::cout << "Sisa gaji setelah dipakai pembayaran listrik Rp. 200.000 dalam sebulan : " << totalGaji << std::endl;
+    
+    totalGaji -= menabung;
+    std::cout << "Sisa gaji setelah menabung : " << totalGaji << std::endl;
+    
+    std::cout << "Sisa gaji : " << totalGaji << std::endl;
     
     
     return 0;
